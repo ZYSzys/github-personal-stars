1ab1c9733b8798fae53371a6aa1c6896b2da0195
ZYSzys <17367077526@163.com>
2018-08-20 19:35:21 +0800

    With ava

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const ora = require('ora')
+/*const ora = require('ora')
 const countPersonalStars = require('.')
 
 const spinner = ora('Testing...')
@@ -8,4 +8,16 @@ const username = 'ZYSzys'
 countPersonalStars(username, (err, total) => {
   spinner.stop()
   console.log(err || (username + ' has ' + total + ' stars totally!'))
+})
+*/
+
+import test from 'ava'
+import countPersonalStars from '.'
+
+test.cb('sort torvalds starred repos', t => {
+  countPersonalStars('torvalds', t.end)
+})
+
+test.cb('sort ZYSzys starred repos', t => {
+  countPersonalStars('ZYSzys', t.end)
 })
